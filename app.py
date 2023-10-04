da609561e9eb4a02fedf7c32a5e04b655eedc064
harsh6812 <harshvardhan.hs.9@gmail.com>
2023-10-04 14:11:51 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import pickle
+st.title("Web Deployment of Medical Diagnostic App ")
+st.subheader("Is the person diabetic ?")
+st.subheader('Ye Kaha AA ')
